201bf54daae7fd92ac5f53910fdb01f9dfc42826
Donal-Noye <leekeiz49@gmail.com>
2025-10-26 20:58:43 +0100

    chor: add heroui

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,10 @@
+import {Button} from "@heroui/react";
+
 function App() {
   return (
     <div className="">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+	    <Button color="primary">Создать</Button>
     </div>
   );
 }
